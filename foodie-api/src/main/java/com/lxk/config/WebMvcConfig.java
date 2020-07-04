3169b022a41e0e96d7af762fb61d2a708d931d65
songshiyu <songshiyu_s@163.com>
2020-07-04 21:55:14 +0800

    为静态资源提供网络映射服务

--- foodie-api/src/main/java/com/lxk/config/WebMvcConfig.java
+++ foodie-api/src/main/java/com/lxk/config/WebMvcConfig.java
@@ -4,17 +4,28 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author songshiyu
  * @date 2020/7/1 22:05
  **/
-
 @Configuration
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer{
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder){
         return builder.build();
+    }
+
+    /**
+     * 实现静态资源的映射
+     * */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/META-INF/resources/")
+                .addResourceLocations("file:///E:\\工作软件\\data\\images\\");
     }
 }
