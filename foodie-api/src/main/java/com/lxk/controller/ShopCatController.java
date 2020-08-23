a5dd378c574a65fdec0264bc6234181b1fed46c2
lxk <songshiyu_s@163.com>
2020-08-23 17:16:25 +0800

    添加和删除购物车时，更新redis中存储内容

--- foodie-api/src/main/java/com/lxk/controller/ShopCatController.java
+++ foodie-api/src/main/java/com/lxk/controller/ShopCatController.java
@@ -1,6 +1,8 @@
 package com.lxk.controller;
 
 import com.lxk.pojo.bo.ShopcartBo;
+import com.lxk.utils.JsonUtils;
+import com.lxk.utils.RedisOperator;
 import com.lxk.utils.ResultJSONResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -8,10 +10,13 @@ import io.swagger.annotations.ApiParam;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author songshiyu
@@ -21,7 +26,10 @@ import javax.servlet.http.HttpServletResponse;
 @Api(value = "购物车接口controller", tags = {"购物车接口相关api"})
 @RestController
 @RequestMapping("shopcart")
-public class ShopCatController {
+public class ShopCatController extends BaseController {
+
+    @Autowired
+    private RedisOperator redisOperator;
 
     private static final Logger logger = LoggerFactory.getLogger(ShopCatController.class);
 
@@ -37,7 +45,34 @@ public class ShopCatController {
             return ResultJSONResult.errorMsg("");
         }
         logger.info(shopcartBo.toString());
-        //TODO 前端用户在登录的情况下，添加商品到购物车，会同时同步购物车到redis缓存
+        //当前端用户在登录的情况下，添加商品到购物车，会同时同步购物车到redis缓存
+        //需要判断当前购物车中已经存在的商品，如果已经存在，则累加数量
+        String shopcartJson = redisOperator.get(FOODIE_SHOPCART + ":" + userId);
+        List<ShopcartBo> shopcartBoList = null;
+        if (StringUtils.isNotBlank(shopcartJson)) {
+            //redis中已经有该用户的购物车
+            shopcartBoList = JsonUtils.jsonToList(shopcartJson, ShopcartBo.class);
+            //判断购物车中是否已有该商品
+            boolean isHaving = false;
+            for (ShopcartBo sc : shopcartBoList) {
+                String tempSpecId = sc.getSpecId();
+                if (tempSpecId.equals(shopcartBo.getSpecId())) {
+                    sc.setBuyCounts(sc.getBuyCounts() + shopcartBo.getBuyCounts());
+                    isHaving = true;
+                }
+            }
+            if (!isHaving) {
+                shopcartBoList.add(shopcartBo);
+            }
+        } else {
+            //redis中没有存在该用户的购物车
+            shopcartBoList = new ArrayList<>();
+            //直接将该商品添加到购物车中
+            shopcartBoList.add(shopcartBo);
+        }
+
+        //覆盖现有redis中该用户的购物车
+        redisOperator.set(FOODIE_SHOPCART + ":" + userId, JsonUtils.objectToJson(shopcartBoList));
 
         return ResultJSONResult.ok();
     }
@@ -53,8 +88,24 @@ public class ShopCatController {
         if (StringUtils.isBlank(userId) || StringUtils.isBlank(itemSpecId)) {
             return ResultJSONResult.errorMsg("未选中商品");
         }
-        logger.info("删除用户：{} 购物车中的商品：{}",userId,itemSpecId);
-        //TODO 前端用户在登录的情况下，删除购物车中商品，会同时同步删除对应的redis缓存
+        logger.info("删除用户：{} 购物车中的商品：{}", userId, itemSpecId);
+        //前端用户在登录的情况下，删除购物车中商品，会同时同步删除对应的redis缓存
+        List<ShopcartBo> shopcartBoList = null;
+
+        String shopcartJson = redisOperator.get(FOODIE_SHOPCART + ":" + userId);
+
+        if (StringUtils.isNotBlank(shopcartJson)) {
+            shopcartBoList = JsonUtils.jsonToList(shopcartJson, ShopcartBo.class);
+            for (ShopcartBo sc : shopcartBoList) {
+                if (sc.getSpecId().equals(itemSpecId)) {
+                    shopcartBoList.remove(sc);
+                    break;
+                }
+            }
+        }
+
+        redisOperator.set(FOODIE_SHOPCART + ":" + userId, JsonUtils.objectToJson(shopcartBoList));
+
         return ResultJSONResult.ok();
     }
 }
