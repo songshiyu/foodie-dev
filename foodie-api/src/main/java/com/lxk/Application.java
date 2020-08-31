0c55e18fcdbb6d3d46bafd488e211675fd9f98ee
lxk <songshiyu_s@163.com>
2020-08-31 21:18:33 +0800

    1.整合spring-session

--- foodie-api/src/main/java/com/lxk/Application.java
+++ foodie-api/src/main/java/com/lxk/Application.java
@@ -2,8 +2,10 @@ package com.lxk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -12,7 +14,8 @@ import tk.mybatis.spring.annotation.MapperScan;
  * @date 2020/6/14 17:52
  **/
 
-@SpringBootApplication
+/**exclude = {SecurityAutoConfiguration.class} 去除spring-boot的自动安全装配*/
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 /**  扫描mybatis通用mapper 所在的包*/
 @MapperScan(basePackages = "com.lxk.mapper")
 /**开启事务管理，默认开启，此处可以不用*/
@@ -21,6 +24,8 @@ import tk.mybatis.spring.annotation.MapperScan;
 @ComponentScan(basePackages = {"com.lxk","org.n3r.idworker"})
 /**开启定时任务*/
 @EnableScheduling
+/** 开启使用redis作为spring session*/
+@EnableRedisHttpSession
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
